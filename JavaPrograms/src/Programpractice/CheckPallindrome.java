a8ccb1a67290dc43ba876705fb608a71dbd68046
Admin <Admin@Satya>
2024-12-03 12:53:46 +0530

    Pallindrome program

--- JavaPrograms/src/Programpractice/CheckPallindrome.java
+++ JavaPrograms/src/Programpractice/CheckPallindrome.java
@@ -4,6 +4,24 @@ public class CheckPallindrome {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int OriginalNumber = 121;
+		int ReversedNumber = 0;
+		int number = OriginalNumber;
+		
+		while(number!=0) {
+			
+			int digit = number % 10;
+			ReversedNumber = ReversedNumber * 10 + digit;
+			number = number/10;
+			//System.out.println(ReversedNumber);
+		}
+		if (ReversedNumber == OriginalNumber) {
+			
+			System.out.println("The entered number is Pallindrome number");
+		}else {
+			
+			System.out.println("The enetered number is not a Pallindrome number");
+		}
 
 	}
 
